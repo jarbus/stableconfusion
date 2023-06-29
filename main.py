2a241b48af72ca1387d6aa20c6e006599aee809b
Jack <jarbus@tutanota.com>
2023-06-29 12:53:41 -0400

    chg: move to fastapi

--- main.py
+++ main.py
@@ -1,71 +1,35 @@
-import os
-import sys
-import multiprocessing as mp
-from multiprocessing import Process, Queue, Manager
-from socketserver import ThreadingTCPServer,StreamRequestHandler
-from threading import Lock
+import io
 import logging as log
-from utils import *
-from worker import *
-from config import *
-setup_logging()
+import base64
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import StreamingResponse
+from fastapi.middleware.cors import CORSMiddleware
+from diffusers import DiffusionPipeline
+from diffusers import DPMSolverMultistepScheduler
 
-#cfg = benchmark1_config
-# cfg = benchmark2_config
-# cfg = benchmark3_config
-cfg = benchmark4_config
+import torch
+model_id = "runwayml/stable-diffusion-v1-5"
+pipeline = DiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16, local_files_only=True)
+# this solver reduces time needed per generation
+pipeline.scheduler = DPMSolverMultistepScheduler.from_config(pipeline.scheduler.config)
 
-processes = []
+pipeline = pipeline.to("cuda")
 
-def setup(iqueue, odict):
-    log.info(f"Loading prompt...")
-    with open(cfg.prompt_file, "r") as f:
-        prompt = f.read()
-    log.info(f"Evaluating initial prompt")
-    llm = make_model(cfg)
-    prompt_tokens = llm.tokenize(prompt.encode())
-    llm.eval(prompt_tokens)
-    state = llm.save_state()
-    log.info(f"Launching {cfg.num_llms} llm workers")
-    for i in range(cfg.num_llms):
-        p = Process(target=launch_worker,
-                    args=[i, prompt, state, iqueue, odict, cfg])
-        p.start()
-        processes.append(p)
+app = FastAPI()
+@app.get("/")
+def submit_prompt(prompt: str):
+    generator = torch.Generator("cuda").manual_seed(0)
+    print(f"Generating image for prompt: {prompt}")
+    image = pipeline(prompt, 
+                     generator=generator,
+                     num_inference_steps=20
+                     ).images[0]
+    image_stream = io.BytesIO()
+    image.save(image_stream, format="JPEG")
+    image_stream.seek(0)
+    # response = StreamingResponse(image_stream, media_type="image/jpeg")
+    print(f"Returning photo for prompt: {prompt}")
+    image_bytes = image_stream.read()
+    image_b64 = base64.b64encode(image_bytes)
 
-def start_webserver(iqueue, odict, qid, qlock, port=8000):
-    class QueryHandler(StreamRequestHandler):
-        def handle(self):
-            log.info(f'Connected: {self.client_address[0]}:{self.client_address[1]}')
-            while True:
-                # get message
-                msg = self.request.recv(4096).strip()#rfile.readline()
-                if not msg:
-                    log.info(f'Disconnected: {self.client_address[0]}:{self.client_address[1]}')
-                    break # exits handler, framework closes socket
-                # lock qid.value, increment it, then unlock
-                qlock.acquire()
-                qid.value += 1
-                v = qid.value
-                qlock.release()
-                output = submit_query(v, msg.decode(), iqueue, odict)
-                print(f'Received: {output}')
-                self.wfile.write(output.encode())
-                self.wfile.flush()
-
-    log.info(f"Starting server on port {port}")
-    server = ThreadingTCPServer(('',port),QueryHandler)
-    server.allow_reuse_address = True
-    server.serve_forever()
-
-
-if __name__ == "__main__":
-    log.info(f"---------STARTING-MAIN-----------")
-    port = int(sys.argv[1])
-    iqueue = Queue()
-    qlock = Lock()
-    with Manager() as manager:
-        odict = manager.dict()
-        qid = manager.Value('i', 0)
-        setup(iqueue, odict)
-        start_webserver(iqueue, odict, qid, qlock, port=port)
+    return image_b64
--- utils.py
+++
@@ -1,4 +0,0 @@
-import logging as log
-def setup_logging():
-    log.basicConfig(filename="main.log",format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S%p')
-    log.getLogger().setLevel(log.INFO)
